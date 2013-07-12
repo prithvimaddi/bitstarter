238b620cc2bac9bf61768db2eae69c04150bdb89
Prithvi Maddi <PrithviMaddi@yahoo.com>
2013-07-12 03:51:05 +0000

    Changed response

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   fs.readFile('index.html', function(err, data){
       response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end(content, 'utf-8');
+      response.end(data,'utf-8');
       //response.send(data);
   })
 });
